6088e63794d9e0d3ee87a228a7e5d0b791780407
Chisom Ikechukwu <ik3chukwuchisom28@gmail.com>
2024-10-05 23:28:42 +0100

    complete prime.py

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
+
+# Function to return the first 'number_of_primes' prime numbers
 def primes(number_of_primes):
     list = []
+    num = 0  
+
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num += 1
+    
     return list
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+print(primes(15))
